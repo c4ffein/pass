bddc60f5620c1b03b41717fe0a3432b4b53ce782
c4ffein <c4ffein@gmail.com>
2025-05-13 23:42:50 +0200

    test-adapter.sh return value

--- test_tricks/test-adapter.sh
+++ test_tricks/test-adapter.sh
@@ -15,13 +15,19 @@ cd temp-test-env/tests
 if [ -n "$1" ]; then  # Run a specific test
     echo "Running test: $1"
     ./$1
+    EXIT_CODE=$?
 else  # Run all tests
+    EXIT_CODE=0
     for test in ./t[0-9][0-9][0-9][0-9]-*.sh; do
         echo "Running test: $test"
         ./$test
+        if [ $? -ne 0 ]; then
+            EXIT_CODE=1
+        fi
     done
 fi
 
 cd ../..
 
 rm -rf temp-test-env
+exit $EXIT_CODE
